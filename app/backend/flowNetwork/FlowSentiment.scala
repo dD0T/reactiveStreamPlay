fa58e330a4adc60c1d50f626d2ca01d81f3f19e8
Stefan Hacker <dd0t@users.sourceforge.net>
2014-07-19 17:46:48 +0200

    Prevent FlowSentiment element from crashing by stubbing it.

--- app/backend/flowNetwork/FlowSentiment.scala
+++ app/backend/flowNetwork/FlowSentiment.scala
@@ -11,5 +11,7 @@ class FlowSentiment(id: Long, name: String,  x: Int, y: Int)
   extends FlowNode(id, name, FlowSentiment.nodeType, x, y, 1, 1) with TargetableFlow with FlowFieldOfInterest {
 
   // Utilizes http://sentiwordnet.isti.cnr.it/
-  override def active: Receive = ???
+  override def active: Receive = {
+    case _ => // Do nothing at all ;)
+  }
 }
