3848b73f63b613e6157a859a8efdbba2d0039806
Stefan Hacker <dd0t@users.sourceforge.net>
2014-07-25 23:04:52 +0200

    Comment and clean up FlowSupervisor

--- app/backend/flowNetwork/FlowSupervisor.scala
+++ app/backend/flowNetwork/FlowSupervisor.scala
@@ -5,13 +5,12 @@ import backend.NextFlowUID
 import backend.flowNetwork.sinks.{FlowAccumulator, FlowFrequency, FlowTrace, FlowCounter}
 import backend.flowNetwork.sources.{FlowTwitterSource, FlowIpsumSource, FlowNumberSource}
 import backend.flowNetwork.transformations._
-import play.api.libs.iteratee.Concurrent.Channel
-import play.api.libs.iteratee.{Concurrent, Enumerator}
+import play.api.libs.iteratee.Concurrent
 import play.api.libs.json.JsValue
-import views.html.helper.input
 
 /** Registers an observer for node and connection configuration updates */
 case class Register(observer: ActorRef)
+/** Unregisters an observer for node and connection configuration updates */
 case class Unregister(observer: ActorRef)
 
 /** Triggers a complete re-discovery of the whole network configuration */
@@ -24,19 +23,50 @@ case class LookupObj(id: Long)
 /** Requests Some[Long] ID of the node with the given ActorRef */
 case class LookupId(obj: ActorRef)
 
-/** Returns a list of possible flow objects */
+/** Requests a list of possible flow objects */
 case object GetFlowObjectTypes
+/** Requests a set of all object IDs */
 case object GetFlowObjects
+
+/** Requests the creation of a flow object.
+  * Answers with Some((id, ActorRef)) on success. None on failure.
+  *
+  * @param what NodeType to create
+  * @param x X-Position in visualisation
+  * @param y Y-Position in visualisation
+  */
 case class CreateFlowObject(what: String, x: Int, y: Int)
+/** Requests deletion of the flow object with the given ID
+  * Answers Some(id) on success, None on failure
+  */
 case class DeleteFlowObject(id: Long)
 
+/** Requests a list of (sourceId, targetId) tuples */
 case object GetConnections
-case class Connect(source: Long, target: Long, attribus: Map[String, String])
-case class Disconnect(source: Long, target: Long)
 
+/** Requests the connection between source and target.
+  * Answers with Some(((sourceId, targetId), connection:ActorRef)) on success. None on failure.
+  *
+  * @param sourceId Source ID
+  * @param targetId Target ID
+  * @param attributes Connection attributes freely settable by the client (e.g. for connector positioning)
+  */
+case class Connect(sourceId: Long, targetId: Long, attributes: Map[String, String])
+
+/** Requests disconnect of sourceId and targetId
+  * Answers with Some((sourceId, targetId)) on success. None on failure.
+  */
+case class Disconnect(sourceId: Long, targetId: Long)
+
+/** Used by FlowSupervisor to request shutdown from its child actors */
 case object Shutdown
+/** Used by child actors to aknowledge supervisor shutdown request */
 case object Stopping
 
+/** Requests a Enumerator[JsValue] subscribed to all network configuration updates.
+  * Receives the full current configuration of the network (nodes then connections)
+  * before streaming of configuration updates starts.
+  */
 case object RequestEnumerator
 
 /** Used for representation and update of the (key -> value) configuration in a Node/Connection */
@@ -48,6 +78,10 @@ object FlowSupervisor {
   def props(): Props = Props(new FlowSupervisor)
 }
 
+/** The FlowSupervisor handles central supervision and control tasks for the Flow network.
+ *  It is the central instance to talk to for network reconfiguration or
+ *  observation.
+ */
 class FlowSupervisor extends Actor with ActorLogging {
 
   val ordinaryFlowObjects = Map[String, (Long, String, Int, Int) => Props](
@@ -66,6 +100,9 @@ class FlowSupervisor extends Actor with ActorLogging {
     FlowStopwordFilter.nodeType -> FlowStopwordFilter.props
   )
 
+  /** Helper object which name sequences when called repeatedly.
+    * e.g. generates Name1, Name2, ...  for newActorName("Name")
+    */
   object newActorName {
     var objectCounts = scala.collection.mutable.Map[String, Int]().withDefaultValue(0)
     def apply(name: String) = {
@@ -87,11 +124,19 @@ class FlowSupervisor extends Actor with ActorLogging {
 
   var observers = scala.collection.mutable.Set.empty[ActorRef]
 
+  /** Returns all connections related to the given node id */
   private def connectionsFor(id: Long): scala.collection.mutable.Map[(Long, Long), ActorRef] =
-    connectionIdsToObj.filter { case ((sourceId, targetId),_) => (sourceId == id || targetId == id) }
+    connectionIdsToObj.filter { case ((sourceId, targetId),_) => sourceId == id || targetId == id }
 
+  /** Broadcasts the given message to all registered observers */
   private def notifyObservers[T](message: T) = observers map { _ ! message }
 
+  /** Removes an existing connection between two nodes.
+    *
+    * @param sourceId Source node ID
+    * @param targetId Target node ID
+    * @return true on success. False on failure.
+    */
   private def disconnect(sourceId: Long, targetId: Long): Boolean = {
     connectionIdsToObj.remove((sourceId, targetId)) match {
       case Some(connection) =>
@@ -147,7 +192,7 @@ class FlowSupervisor extends Actor with ActorLogging {
           currentConnectionConfigurations map { case (k,v) => translator ! (k,v) }
         },
         onComplete = { shutdownTranslator() },
-        onError = (_,_) => shutdownTranslator
+        onError = (_,_) => shutdownTranslator()
       ).onDoneEnumerating(
           callback = shutdownTranslator
       )
@@ -155,11 +200,11 @@ class FlowSupervisor extends Actor with ActorLogging {
       sender() ! enumerator
 
     case Register(observer) =>
-      log.info(s"${observer} registered for updates")
+      log.info(s"$observer registered for updates")
       observers += observer
 
     case Unregister(observer) =>
-      log.info(s"${observer} removed from updates")
+      log.info(s"$observer removed from updates")
 
     case DetectConfiguration =>
       log.info(s"${sender()} triggered configuration update")
@@ -169,7 +214,7 @@ class FlowSupervisor extends Actor with ActorLogging {
 
     case CreateFlowObject(objectType, x, y) =>
       ordinaryFlowObjects.get(objectType) match {
-        case Some(_) => {
+        case Some(_) =>
           log.info(s"Creating new $objectType for ${
             sender()
           }")
@@ -181,7 +226,7 @@ class FlowSupervisor extends Actor with ActorLogging {
 
           obj ! GetConfiguration // Pull configuration
           sender() ! Some((id, obj))
-        }
+
         case None =>
           log.warning(s"Asked to create unknown flow object type $objectType")
           None
@@ -253,23 +298,16 @@ class FlowSupervisor extends Actor with ActorLogging {
         case None => sender() ! None
       }
 
-    /**
-     * Returns a set of sourceID, targetID of all connections
-     */
     case GetConnections =>
       sender() ! connectionIdsToObj.keySet.toSet
 
-    /**
-     * Returns a set of all object IDs
-     */
     case GetFlowObjects =>
       sender() ! flowIdToObject.keySet.toSet
 
     case Connect(sourceId, targetId, attributes) =>
       (flowIdToObject.get(sourceId), flowIdToObject.get(targetId)) match {
-        case (Some(source), Some(target)) if !connectionIdsToObj.contains {
-          (sourceId, targetId)
-        } => {
+        case (Some(source), Some(target)) if !connectionIdsToObj.contains { (sourceId, targetId) } =>
+
           log.info(s"Creating new connection from $source to $target")
           val connection = context.actorOf(
             FlowConnection.props(source, sourceId, target, targetId, attributes),
@@ -282,7 +320,6 @@ class FlowSupervisor extends Actor with ActorLogging {
           sender() ! Some(((sourceId, targetId), connection))
           connection ! GetConfiguration
 
-        }
         case _ =>
           sender ! None
           log.warning(s"Asked to connect $sourceId with $targetId of which are invalid or already connected")
